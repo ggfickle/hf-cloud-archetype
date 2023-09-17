a0c6eb0c261f671f154e33f318746e03f4490a18
xiehongfei <hoongfei@qq.com>
2023-09-17 18:48:09 +0800

    code commit

--- test/src/test/java/com/hf/biz/test/BaseTest.java
+++ test/src/test/java/com/hf/biz/test/BaseTest.java
@@ -1,21 +1,21 @@
-package com.hf.biz.test;
-
-import com.hf.biz.web.Application;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import javax.persistence.EntityManager;
-
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = Application.class)
-public class BaseTest {
-
-    @Autowired
-    public JPAQueryFactory jpaQueryFactory;
-
-    @Autowired
-    public EntityManager entityManager;
-}
+//package com.hf.biz.test;
+//
+//import com.hf.biz.web.Application;
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//import javax.persistence.EntityManager;
+//
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest(classes = Application.class)
+//public class BaseTest {
+//
+//    @Autowired
+//    public JPAQueryFactory jpaQueryFactory;
+//
+//    @Autowired
+//    public EntityManager entityManager;
+//}
